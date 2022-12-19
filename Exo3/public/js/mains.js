7effa490a3fd510694be87b5f3ac49ac06f9859c
Quent_DLM <qdelameilleure@gmail.com>
2022-12-19 14:08:55 +0100

    commentaire pour m'aider plus tard

--- Exo3/public/js/mains.js
+++ Exo3/public/js/mains.js
@@ -10,8 +10,16 @@ let resultatAddition = Number(chiffre1) + Number(chiffre2);
 let resultatSoustraction = Number(chiffre1) - Number(chiffre2);
 let resultatDivision = Number(chiffre1) / Number(chiffre2);
 let resultatMultiplication = Number(chiffre1) * Number(chiffre2);
+//peut etre fais avec des $?//
 
 alert(chiffre1 + " " + addition + " " + chiffre2 + " = " + resultatAddition + "\n" +
     chiffre1 + " " + soustraction + " " + chiffre2 + " = " + resultatSoustraction + "\n" +
     chiffre1 + " " + division + " " + chiffre2 + " = " + resultatDivision + "\n" +
     chiffre1 + " " + multiplication + " " + chiffre2 + " = " + resultatMultiplication);
+    // oui : exemple //
+// let message = `${chiffre1} ${addition} ${chiffre2} = ${resultatAddition}
+//${chiffre1} ${soustraction} ${chiffre2} = ${resultatSoustraction}
+//${chiffre1} ${division} ${chiffre2} = ${resultatDivision}
+//${chiffre1} ${multiplication} ${chiffre2} = ${resultatMultiplication}`;
+
+//alert(message);**  **//
