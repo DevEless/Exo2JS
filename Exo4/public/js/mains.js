b3d4714959648750b330b327de59f174d5f3ae7b
Quent_DLM <qdelameilleure@gmail.com>
2022-12-19 14:04:06 +0100

    commentaire pour m'aider plus tard

--- Exo4/public/js/mains.js
+++ Exo4/public/js/mains.js
@@ -1,5 +1,7 @@
 let phrase = prompt("Saisissez une phrase :");
 let motAChercher = prompt("Quel mot de la phrase voulez-vous remplacer ?");
 let motRemplacement = prompt("Par quel mot voulez-vous remplacer ce mot ?");
+
+//remplace le mot a chercher par le mot de remplacement//
 let phraseModifiee = phrase.replace(motAChercher, motRemplacement);
 alert("Voici la phrase modifiée : " + phraseModifiee);
