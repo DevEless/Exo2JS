40b9d64b50d4cae313cfd3d233542d405f5807e8
Quent_DLM <qdelameilleure@gmail.com>
2022-12-19 13:52:19 +0100

    exo1

---
+++ Exo1/public/js/main.js
@@ -0,0 +1,9 @@
+let prenom = "Mon prénom est Quentin";
+console.log(prenom);
+
+let age = 23;
+console.log(age);
+
+let presentation = "Bonjour, je m'appelle Marie et j'ai 25 ans.";
+console.log(presentation);
+alert(presentation);
