4eb052d3af74c4c7f71c4a38cf03a3b68b136554
Quent_DLM <qdelameilleure@gmail.com>
2022-12-19 13:54:28 +0100

    exo2

---
+++ Exo2/public/js/mains.js
@@ -0,0 +1,4 @@
+let prenom = prompt("Quel est votre prénom ?");
+let age = prompt("Quel est votre age ?");
+let presentation = "Bonjour, je m'appelle " + prenom + " et j'ai " + age + " ans.";
+alert(presentation);
